f53d7cfd1d126ba47613d5d60f6d00c1872b6a39
iginfantes <iginfantes@minsait.com>
2022-06-18 20:34:03 +0200

    añadiendo modulos sencillos

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports={
+    hola:()=>{
+        console.log('Hola Mundo');
+    },
+    holaPersonalizado: (nombre)=>{
+        console.log(`Hola ${ nombre }`);
+    },
+    holaAlCurosNPM:()=>{
+        console.log('Hola al curso NPM!!!');
+    }
+}
